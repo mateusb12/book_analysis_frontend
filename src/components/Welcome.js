1671447260f11accb1c390b5e11bef90377845da
Mateus <matbessa12@gmail.com>
2024-06-12 11:09:55 -0300

    Enhanced Welcome Component and CSS Styling - Imported and utilized specific image assets in `Welcome.js` for each step. - Added unique IDs and class to images in `Welcome.js` for better CSS targeting. - Extended `Welcome.css` with specific styles for title and step images, and adjusted `.welcome-background` to have a minimum height.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../css/Welcome.css';
+import bookImage from '../assets/img/book.png';
+import csvTable from '../assets/img/csv_table.png';
+import entitiesTable from '../assets/img/entities_table.png';
+import filteredTable from '../assets/img/filtered_table.png';
 import { NavBar } from "./NavBar";
 
 export const Welcome = () => {
@@ -7,26 +11,26 @@ export const Welcome = () => {
         <div className="welcome-background">
             <NavBar />
             <main className="main-content">
-                <h1>How does it work?</h1>
+                <h1 className="title">How does it work?</h1>
                 <Step
                     stepNumber="1"
                     stepTitle="Get a .txt book"
-                    imageUrl='../assets/img/book.png'
+                    imageUrl={bookImage}
                 />
                 <Step
                     stepNumber="2"
                     stepTitle="Get a .csv character table"
-                    imageUrl="url-to-your-image"
+                    imageUrl={csvTable}
                 />
                 <Step
                     stepNumber="3"
                     stepTitle="Apply NLP to get the book entities"
-                    imageUrl="url-to-your-image"
+                    imageUrl={entitiesTable}
                 />
                 <Step
                     stepNumber="4"
                     stepTitle="Use the table to filter character-only entities"
-                    imageUrl="url-to-your-image"
+                    imageUrl={filteredTable}
                 />
             </main>
         </div>
@@ -34,10 +38,11 @@ export const Welcome = () => {
 };
 
 const Step = ({ stepNumber, stepTitle, imageUrl }) => {
+    const stepId = `step-image-${stepNumber}`;
     return (
         <div className="step">
             <h2>{stepNumber}. {stepTitle}</h2>
-            <img src={imageUrl} alt={stepTitle} />
+            <img src={imageUrl} alt={stepTitle} id={stepId} className="step-image"/>
         </div>
     );
 };
