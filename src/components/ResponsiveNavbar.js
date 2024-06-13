af5e59057094bf6e3da019f97ca9018f289f2dfe
Mateus <matbessa12@gmail.com>
2024-06-13 19:07:23 -0300

    aaaaaaaaaaaaaaaaa

--- src/components/ResponsiveNavbar.js
+++ src/components/ResponsiveNavbar.js
@@ -2,8 +2,8 @@ import '../css/ResponsiveNavbar.css';
 
 export const ResponsiveNavBar = () => {
     return (
-        <nav className="navbar-light bg-primary">
-            Styling!
+        <nav className="navbar-light bg-light">
+            <span className="navbar-brand">Title!</span>
         </nav>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
